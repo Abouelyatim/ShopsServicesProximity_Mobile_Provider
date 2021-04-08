8214f0f11b851ab38d1876e3623df194b8182702
unknown <gi_berkane@esi.dz>
2021-04-08 17:14:46 +0100

    feat notification settings (sound & vibration)

--- app/src/main/java/com/smartcity/provider/ui/main/order/notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/smartcity/provider/ui/main/order/notification/MyFirebaseMessagingService.kt
@@ -5,6 +5,11 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.smartcity.provider.util.NotificationSettings.Companion.ORDERS_NOTIFICATION
+import com.smartcity.provider.util.NotificationSettings.Companion.SOUND_NOTIFICATION
+import com.smartcity.provider.util.NotificationSettings.Companion.VIBRATION_NOTIFICATION
+import com.smartcity.provider.util.PreferenceKeys
+
 
 object Events {
     val serviceEvent: MutableLiveData<String> by lazy {
@@ -32,15 +37,35 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun handleCustomDataMessage(title: String, message: String) {
-        showNotificationMessage(applicationContext, title, message)
+        val preferences = baseContext.getSharedPreferences(
+            PreferenceKeys.APP_PREFERENCES,
+            Context.MODE_PRIVATE
+        )
+        val settings=preferences.getStringSet(PreferenceKeys.NOTIFICATION_SETTINGS,null)
+        settings?.let {
+            if(ORDERS_NOTIFICATION in it){
+
+                showNotificationMessage(
+                    this,
+                    title,
+                    message,
+                    SOUND_NOTIFICATION in it,
+                    VIBRATION_NOTIFICATION in it
+                )
+
+            }
+        }
+
     }
 
     private fun showNotificationMessage(
         context: Context,
         title: String,
-        message: String
+        message: String,
+        shouldSound:Boolean,
+        shouldVibrate:Boolean
     ) {
         notificationUtils = NotificationUtils(context)
-        notificationUtils?.showNotificationMessage(title, message)
+        notificationUtils?.showNotificationMessage(title, message,shouldSound,shouldVibrate)
     }
 }
--- app/src/main/java/com/smartcity/provider/ui/main/order/notification/NotificationUtils.kt
+++ app/src/main/java/com/smartcity/provider/ui/main/order/notification/NotificationUtils.kt
@@ -1,70 +1,144 @@
 package com.smartcity.provider.ui.main.order.notification
 
-import android.R
+import android.annotation.TargetApi
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.ContextWrapper
-import android.content.res.Resources
-import android.graphics.Color
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
+import com.smartcity.provider.R
+import com.smartcity.provider.ui.main.MainActivity
 import kotlin.random.Random
 
-class NotificationUtils(base: Context) : ContextWrapper(base) {
 
-    val   ANDROID_CHANNEL_ID:String  = "com.smartcity.provider.ui.main.order.notification"
-    val   ANDROID_CHANNEL_NAME:String  = "ANDROID CHANNEL"
+class NotificationUtils(val base: Context) : ContextWrapper(base) {
     var NOTIFICATION_ID = 100
 
     init {
         NOTIFICATION_ID= Random.nextInt()
     }
 
+
     fun showNotificationMessage(
-        title: String?,
-        body: String?
+        title:String,
+        body:String,
+        shouldSound:Boolean,
+        shouldVibrate:Boolean
     ){
-        val mBuilder = NotificationCompat.Builder(
-            applicationContext,
-            ANDROID_CHANNEL_ID
-        )
-
-        val notification=
-            mBuilder
-                .setContentTitle(title)
-                .setContentText(body)
-                .setSmallIcon(R.drawable.stat_notify_more)
-                .setAutoCancel(true)
-                .build()
-
-        createChannels()
-        getManager().notify(NOTIFICATION_ID,notification)
-    }
-
-    fun createChannels() {
-        // create android channel
-         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-             val androidChannel: NotificationChannel = NotificationChannel(
-                ANDROID_CHANNEL_ID,
-                ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH
-            )
-            // Sets whether notifications posted to this channel should display notification lights
-            androidChannel.enableLights(true);
-            // Sets whether notification posted to this channel should vibrate.
-            androidChannel.enableVibration(true);
-            // Sets the notification light color for notifications posted to this channel
-            androidChannel.setLightColor(Color.GREEN);
-            // Sets whether notifications posted to this channel appear on the lockscreen or not
-            androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-            getManager().createNotificationChannel(androidChannel)
-
+        if (isOreoOrAbove()) {
+            setupNotificationChannels()
         }
+        makeNotification(title,body,shouldSound,shouldVibrate)
     }
 
     fun  getManager(): NotificationManager {
         return getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.O)
+    fun registerNormalNotificationChannel(notificationManager: NotificationManager) {
+        val channel_all = NotificationChannel(
+            "CHANNEL_ID_ALL",
+            "CHANNEL_NAME_ALL",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel_all.enableLights(true)
+        channel_all.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+
+        channel_all.enableVibration(true)
+        notificationManager.createNotificationChannel(channel_all)
+        val channel_sound = NotificationChannel(
+            "CHANNEL_ID_SOUND",
+            "CHANNEL_NAME_ALL",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel_sound.enableVibration(false)
+        notificationManager.createNotificationChannel(channel_sound)
+        val channel_vibrate = NotificationChannel(
+            "CHANNEL_ID_VIBRATE",
+            "CHANNEL_NAME_ALL",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel_vibrate.setSound(null, null)
+        channel_vibrate.enableVibration(true)
+        notificationManager.createNotificationChannel(channel_vibrate)
+        val channel_none = NotificationChannel(
+            "CHANNEL_ID_NONE",
+            "CHANNEL_NAME_ALL",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel_none.setSound(null, null)
+        channel_none.enableVibration(false)
+        notificationManager.createNotificationChannel(channel_none)
+    }
+
+    private fun setupNotificationChannels() {
+        registerNormalNotificationChannel(getManager())
+    }
+
+    private fun playSound(){
+        val mediaPlayer = MediaPlayer.create(base, R.raw.notification)
+        mediaPlayer.start()
+    }
+
+    fun makeNotification(
+        title:String,
+        body:String,
+        shouldSound:Boolean,
+        shouldVibrate:Boolean
+    ) {
+        val builder: NotificationCompat.Builder =
+            NotificationCompat.Builder(base, getChannelId(shouldSound,shouldVibrate))
+                .setContentTitle(title)
+                .setSmallIcon(R.mipmap.ic_stat_notify_more)
+                .setContentText(body)
+
+        val intent = Intent(base, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            base,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        builder.setContentIntent(pendingIntent)
+
+        if (shouldSound && !shouldVibrate) {
+            playSound()
+            builder.setDefaults(Notification.DEFAULT_SOUND)
+                .setVibrate(longArrayOf(0L))
+        }
+        if (shouldVibrate && !shouldSound) {
+            builder.setDefaults(Notification.DEFAULT_VIBRATE)
+                .setSound(null)
+        }
+        if (shouldSound && shouldVibrate) {
+            playSound()
+            builder.setDefaults(Notification.DEFAULT_ALL)
+        }
+
+        getManager().notify(NOTIFICATION_ID, builder.build())
+    }
+    private fun getChannelId(
+        shouldSound:Boolean,
+        shouldVibrate:Boolean
+    ): String {
+        return if (shouldSound && shouldVibrate) {
+            "CHANNEL_ID_ALL"
+        } else if (shouldSound && !shouldVibrate) {
+            "CHANNEL_ID_SOUND"
+        } else if (!shouldSound && shouldVibrate) {
+            "CHANNEL_ID_VIBRATE"
+        } else {
+            "CHANNEL_ID_NONE"
+        }
+    }
+    private fun isOreoOrAbove(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
     }
 }
